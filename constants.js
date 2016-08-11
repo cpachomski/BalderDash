d1faffad0828b00ba694700d4a45380436585a9f
Chris Pachomski <cpachomski@gmail.com>
2016-08-11 13:46:53 -0400

    add constants file for converting time into english

---
+++ constants.js
@@ -0,0 +1,33 @@
+digits = [
+	'One',
+	'Two',
+	'Three',
+	'Four',
+	'Five',
+	'Six',
+	'Seven',
+	'Eight',
+	'Nine',
+	'Ten'
+];
+
+teens = [
+ 	'Eleven',
+ 	'Twelve',
+ 	'Thirteen',
+ 	'Fourteen',
+ 	'Fifteen',
+ 	'Sixteen',
+ 	'Seventeen',
+ 	'Eighteen',
+ 	'Nineteen'
+];
+
+tens = [
+	'',
+	'twenty',
+	'thirty',
+	'forty',
+	'fifty'
+];
+
