f3648946827193e8754061be5e65cefed1ce17f7
Chris Pachomski <cpachomski@gmail.com>
2016-08-11 13:20:31 -0400

    get updateTime working

--- balderDash.js
+++ balderDash.js
@@ -1,3 +1,31 @@
 (function(){
-	console.log('balderdash!');
+
+
+
+
+	function setTime() {
+		var currentTime = new Date();
+		var mer = 'am'
+		var h = currentTime.getHours();
+		var m = currentTime.getMinutes();
+
+		if (h > 12) {
+			h -= 12;
+			mer = 'pm';
+		};
+
+		document.getElementById('time--hour').innerHTML = h;
+		document.getElementById('time--minutes').innerHTML = m;
+		document.getElementById('time--meridian').innerHTML = mer;
+	};
+
+
+
+	setTime();
+	setInterval(function() {
+		setTime();
+	}, 1000)
+
+		
+
 })();
